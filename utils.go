2c912d7ff9d919fc2fc8b04a413a62cdfd57bbdd
Jason <wangshuang@jiayunhui.com>
2018-08-21 10:41:15 +0800

    add utils

---
+++ utils.go
@@ -0,0 +1,23 @@
+package HttpRequest
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+func Export(v interface{}) string {
+	b, err := json.Marshal(v)
+	if err != nil {
+		return ""
+	}
+	var buf bytes.Buffer
+	err = json.Indent(&buf, b, "", "\t")
+	if err != nil {
+		return ""
+	}
+	return buf.String()
+}
+
+func Json(v interface{}) string {
+	return Export(v)
+}
