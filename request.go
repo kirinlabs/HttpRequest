1386fe5604abaf8b46e7546107c9d698876a9fd7
kirinlabs <microlife@126.com>
2019-06-25 15:50:52 +0800

    update

--- request.go
+++ request.go
@@ -242,8 +242,8 @@ func (r *Request) log() {
 	if r.debug {
 		fmt.Printf("[HttpRequest]\n")
 		fmt.Printf("-------------------------------------------------------------------\n")
-		fmt.Printf("Request: %s %s\nHeaders: %v\nCookies: %v\nTimeout: %ds\nReqBody: %v\n", r.method, r.url, r.headers, r.cookies, r.timeout, r.data)
-		fmt.Printf("-------------------------------------------------------------------\n\n")
+		fmt.Printf("Request: %s %s\nHeaders: %v\nCookies: %v\nTimeout: %ds\nReqBody: %v\n\n", r.method, r.url, r.headers, r.cookies, r.timeout, r.data)
+		//fmt.Printf("-------------------------------------------------------------------\n\n")
 	}
 }
 
